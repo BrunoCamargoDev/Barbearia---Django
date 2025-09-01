4f4d766b57bec4e1421a1bfd8b4bb17bba6a0d5e
BrunoCamargoDev <bcamargo414@gmail.com>
2025-09-01 20:10:58 -0300

    Ajustes env

--- barbearia/settings.py
+++ barbearia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = environ.Env().str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = environ.Env().list("ALLOWED_HOSTS")
 
 
 # Application definition
